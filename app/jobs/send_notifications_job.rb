f61f632d09cb814a778c1ad69b9f9a342d485315
parthbpatel <parthtech3010@gmail.com>
2019-12-09 15:40:19 +0530

    setup active job, added sidekiq gem and configuration, added view for article, added routes

---
+++ app/jobs/send_notifications_job.rb
@@ -0,0 +1,9 @@
+class SendNotificationsJob < ApplicationJob
+  self.queue_adapter = :resque
+
+  queue_as :default
+
+  def perform(article)
+    UserMailer.send_notification_mail(article).deliver_now
+  end
+end
